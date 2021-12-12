782218f94b58bb29b9d20dccc720782f8d9fa94c
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2021-12-13 01:50:36 +0200

    todays work

---
+++ subwayCounter/index.js
@@ -0,0 +1,29 @@
+// document.getElementById("count-el").innerText = 5;
+/*
+let firstGroup = 5;
+let secondGroup = 7;
+
+let count = firstGroup + secondGroup; // here we are storing the number of the passangers that entered
+
+console.log(count);
+*/
+
+/* this calculates my dog age! */
+/*
+let myAge = 24;
+let humanDogRatio = 7;
+
+let myDogAge = Math.floor(myAge / humanDogRatio);
+
+console.log(myDogAge)
+*/
+
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+	count++;
+	countEl.innerText = count;
+};
+
