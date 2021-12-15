d9fd02fc24e1d67357b3afaa4dcec099786b66ae
Giovanni Carboni <giovanni.carboni.1997@gmail.com>
2021-12-15 17:36:03 +0200

    some changes

--- subwayCounter/index.js
+++ subwayCounter/index.js
@@ -1,26 +1,8 @@
-// document.getElementById("count-el").innerText = 5;
-/*
-let firstGroup = 5;
-let secondGroup = 7;
 
-let count = firstGroup + secondGroup; // here we are storing the number of the passangers that entered
-
-console.log(count);
-*/
-
-/* this calculates my dog age! */
-/*
-let myAge = 24;
-let humanDogRatio = 7;
-
-let myDogAge = Math.floor(myAge / humanDogRatio);
-
-console.log(myDogAge)
-*/
 
 let countEl = document.getElementById("count-el");
-let countGr = document.getElementById("saved-groups");
-
+// let countGr = document.getElementById("saved-groups");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
 function increment() {
@@ -30,9 +12,14 @@ function increment() {
 
 let savedGroups = [];
 
-function save() {
+/*function save() {
 	savedGroups.shift();
 	savedGroups.unshift(count);
 	countGr.innerText = `${savedGroups} people entered last.`;
-}
+}*/
 
+function save() {
+	// let previousEntries = " " + count + "; ";
+	let previousEntries = ` ${count}; `;
+	saveEl.innerText += previousEntries;
+}
